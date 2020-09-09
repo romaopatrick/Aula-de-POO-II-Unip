1db7fbe680522bc54bba7d14c74eb89514204080
Patrick Romao <patrickromao2001@gmail.com>
2020-09-08 21:48:31 -0300

    Forms da aula 02
    esse forms, sugerido pelo professor, mostra o meu nome, o curso, a instituição de ensino e o semestre em que estou

---
+++ Exercicios/Aula02TeoriaWforms/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Aula02TeoriaWforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btmClickHere = new System.Windows.Forms.Button();
+            this.lblHW = new System.Windows.Forms.Label();
+            this.lblCursoCampus = new System.Windows.Forms.Label();
+            this.lblSemestre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btmClickHere
+            // 
+            resources.ApplyResources(this.btmClickHere, "btmClickHere");
+            this.btmClickHere.Name = "btmClickHere";
+            this.btmClickHere.UseVisualStyleBackColor = true;
+            this.btmClickHere.Click += new System.EventHandler(this.btmClickHere_Click);
+            // 
+            // lblHW
+            // 
+            resources.ApplyResources(this.lblHW, "lblHW");
+            this.lblHW.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblHW.Name = "lblHW";
+            this.lblHW.UseCompatibleTextRendering = true;
+            // 
+            // lblCursoCampus
+            // 
+            resources.ApplyResources(this.lblCursoCampus, "lblCursoCampus");
+            this.lblCursoCampus.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblCursoCampus.Name = "lblCursoCampus";
+            this.lblCursoCampus.UseCompatibleTextRendering = true;
+            // 
+            // lblSemestre
+            // 
+            resources.ApplyResources(this.lblSemestre, "lblSemestre");
+            this.lblSemestre.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblSemestre.Name = "lblSemestre";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.lblSemestre);
+            this.Controls.Add(this.lblCursoCampus);
+            this.Controls.Add(this.lblHW);
+            this.Controls.Add(this.btmClickHere);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btmClickHere;
+        private System.Windows.Forms.Label lblHW;
+        private System.Windows.Forms.Label lblCursoCampus;
+        private System.Windows.Forms.Label lblSemestre;
+    }
+}
+
---
+++ Exercicios/Aula02TeoriaWforms/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula02TeoriaWforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btmClickHere_Click(object sender, EventArgs e)
+        {
+            lblHW.Text = "Patrick Romão Hernandez";
+            lblCursoCampus.Text = "ADS - UNIP Anchieta";
+            lblSemestre.Text = "3º Semestre";
+        }
+    }
+}
